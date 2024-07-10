38f548ab3759092c85353e4d96c5a9feea9f698b
Sunny Eyles <sunnyeyles@Sunnys-MacBook-Air.fritz.box>
2024-07-10 18:46:07 +0200

    update db

--- ds_server/src/helpers/connectToDatabase.ts
+++ ds_server/src/helpers/connectToDatabase.ts
@@ -4,7 +4,8 @@ dotenv.config()
 export const connectToDatabase = async () => {
   const DB_USER = process.env.MONGO_INITDB_ROOT_USERNAME
   const DB_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD
-  const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@sapesu.tmdndf1.mongodb.net/`
+  const uri = `mongodb+srv://deploy_manager:YkqvZ5UtYOIpTJ7I@sapesu.tmdndf1.mongodb.net/`
+  // const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@sapesu.tmdndf1.mongodb.net/`
 
   try {
     await mongoose.connect(uri)
---
+++ ssh_in.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+KEY_FILE="./sapesu_key_pair.pem"
+
+# ssh -i "$KEY_FILE" ubuntu@13.37.240.254
+
+
+
+# ssh -i "/Users/sunnyeyles/.ssh" ubuntu@ec2-13-37-240-254.eu-west-3.compute.amazonaws.com
+
+
+
+
