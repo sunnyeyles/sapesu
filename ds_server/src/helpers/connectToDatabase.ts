933f2458ba5397249bafff0c6158413592844349
sunnyeyles <88967314+sunnyeyles@users.noreply.github.com>
2024-07-12 17:01:23 +0200

    Update connectToDatabase.ts

--- ds_server/src/helpers/connectToDatabase.ts
+++ ds_server/src/helpers/connectToDatabase.ts
@@ -2,10 +2,6 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config()
 export const connectToDatabase = async () => {
-  const DB_USER = process.env.MONGO_INITDB_ROOT_USERNAME
-  const DB_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD
-  const uri = `mongodb+srv://deploy_manager:YkqvZ5UtYOIpTJ7I@sapesu.tmdndf1.mongodb.net/`
-  // const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@sapesu.tmdndf1.mongodb.net/`
 
   try {
     await mongoose.connect(uri)
